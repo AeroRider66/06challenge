828ff7efb0c7be86769402365aed54e25c45e8ed
AeroRider <dave.butterworth.ee@gmail.com>
2024-08-19 19:32:59 -0500

    remove excess notes & code

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,6 +11,7 @@ const error = document.getElementById('error');
 const units = 'imperial'; //can be imperial or metric
 let temperatureSymobol = units == 'imperial' ? "°F" : "°C";
 
+//to get weather for a given city
 async function fetchWeather() {
     try {
         weatherContainer.innerHTML = '';
@@ -33,9 +34,9 @@ async function fetchWeather() {
             return;
         }
         //Display weather data for each 3 hour increment
-        data.list.forEach(hourlyWeatherData => {
-            const hourlyWeatherDataDiv = createWeatherDescription(hourlyWeatherData);
-            weatherContainer.appendChild(hourlyWeatherDataDiv);
+        data.list.forEach(dailyWeatherData => {
+            const hourlyWeatherDataDiv = createWeatherDescription(dailyWeatherData);
+            weatherContainer.appendChild(dailyWeatherDataDiv);
         });
 
         // Display city name based on latitude and longitude
@@ -45,36 +46,3 @@ async function fetchWeather() {
         console.log(error);
     }
 }
-
-function convertToLocalTime(dt) {
-
-    // Create a new Date object by multiplying the Unix timestamp by 1000 to convert it to milliseconds
-    // Will produce a time in the local timezone of user's computer
-    const date = new Date(dt * 1000);
-
-    const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are 0-based, so add 1
-    const day = String(date.getDate()).padStart(2, '0');
-    const hours = String(date.getHours() % 12 || 12).padStart(2, '0'); // Convert 24-hour to 12-hour format
-    const minutes = String(date.getMinutes()).padStart(2, '0');
-    const seconds = String(date.getSeconds()).padStart(2, '0');
-    const period = date.getHours() >= 12 ? 'PM' : 'AM'; // Determine AM/PM
-
-    // Formatted date string in the format: YYYY-MM-DD hh:mm:ss AM/PM
-    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds} ${period}`;
-
-}
-
-function createWeatherDescription(weatherData) {
-    const { main, dt } = weatherData;
-
-    const description = document.createElement("div");
-    const convertedDateAndTime = convertToLocalTime(dt);
-
-    // '2023-11-07 07:00:00 PM'
-    description.innerHTML = `
-        <div class = "weather_description">${main.temp}${temperatureSymobol} - ${convertedDateAndTime.substring(10)} - ${convertedDateAndTime.substring(5, 10)} </div>
-    `;
-    return description;
-}
-
